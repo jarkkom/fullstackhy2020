0a270db528e4f5b08c703d8c6573d9c93d0684ba
Jarkko Mourujarvi <jarkko@mourujarvi.net>
2020-03-22 13:27:31 +0200

    1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,14 +2,26 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  console.log(props);
+  return (
+    <>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good * 1 + bad * -1) / (good + neutral + bad)}</div>
+      <div>positive {good / (good + neutral + bad)}</div>
+    </>
+  )
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-
 
   return (
     <div>
@@ -18,12 +30,7 @@ const App = () => {
         <button onClick={() => { setNeutral(neutral + 1); }}>neutral</button>
         <button onClick={() => { setBad(bad + 1); }}>bad</button>
         <h1>statistics</h1>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {good + neutral + bad}</div>
-        <div>average {(good * 1 + bad * -1) / (good + neutral + bad)}</div>
-        <div>positive {good / (good + neutral + bad)}</div>
+        <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
