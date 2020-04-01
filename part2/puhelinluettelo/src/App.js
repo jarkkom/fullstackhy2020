b9dd1c0f897153d36422a602aea454e03c72a970
Jarkko Mourujarvi <jarkko@mourujarvi.net>
2020-04-01 20:21:31 +0300

    2.9*: puhelinluettelo step4

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
+  
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
+  const [ filter, setFilter ] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -31,6 +36,9 @@ const App = () => {
   const onChangeNewNumber = (event) => {
     setNewNumber(event.target.value);
   }
+  const onChangeFilter = (event) => {
+    setFilter(event.target.value);
+  }
 
   return (
     <div>
@@ -47,13 +55,21 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>
+        <input value={filter} onChange={onChangeFilter} />
+      </div>
+      <div>
         <ul>
           {persons.map((person) => {
-            return (
-              <li key={person.name}>{person.name} {person.number}</li>
-            );
+            if (filter === '' || person.name.toLowerCase().includes(filter.toLowerCase())) {
+              return (
+                <li key={person.name}>{person.name} {person.number}</li>
+              );  
+            } 
+            return false;
           })}
         </ul>
+      </div>
     </div>
   )
 }
