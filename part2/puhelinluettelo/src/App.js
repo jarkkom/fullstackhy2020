05cc537043dfada33c63ad7dd8656dfe9107096b
Jarkko Mourujarvi <jarkko@mourujarvi.net>
2020-04-02 20:05:49 +0300

    2.11: puhelinluettelo step6

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ filter, setFilter }) => {
   const onChangeFilter = (event) => {
@@ -74,15 +75,17 @@ const Persons = ({ persons, filter}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((resp) => {
+        setPersons(resp.data);
+      })
+  }, []);
   
   const [ filter, setFilter ] = useState('');
-
 
   return (
     <div>
