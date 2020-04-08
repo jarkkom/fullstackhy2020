8190ea0adb56005b74ede41410c745e450bb9c5a
Jarkko Mourujarvi <jarkko@mourujarvi.net>
2020-04-08 23:07:28 +0300

    Add CORS support

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 let notes = [
   { 
@@ -78,7 +81,7 @@ app.post('/api/notes', (req, res) => {
   res.json(note);
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
