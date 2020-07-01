f9e9dae7937af132f04a06c26b10baaff33d73e2
Jarkko Mourujärvi <jarkko@mourujarvi.net>
2020-07-01 16:11:31 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- part4/blogilista/tests/list_helper.test.js
+++ part4/blogilista/tests/list_helper.test.js
@@ -59,3 +59,39 @@ describe('total likes', () => {
     expect(result).toBe(5 + 15 + 155);
   });
 });
+
+describe('favoriteBlog', () => {
+  const blogList = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 15,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'Go To Statement Considered Harmful 3',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 155,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Go To Statement Considered Harmful 2',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+    },
+  ];
+
+  test('returns undefined on empty list', () => {
+    const result = listHelper.favoriteBlog([]);
+    expect(result).toEqual(undefined);
+  });
+
+  test('finds blog with most likes', () => {
+    const result = listHelper.favoriteBlog(blogList);
+    expect(result).toEqual(blogList[1]);
+  });
+});
--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -6,8 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  let favorite = blogs[0];
+
+  blogs.forEach(blog => {
+    if (blog.likes > favorite.likes) {
+      favorite = blog;
+    }
+  });
+  return favorite;
+};
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
