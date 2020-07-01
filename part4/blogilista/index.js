01aea22626e3a8d71b3911f647926f3e0d96572a
Jarkko Mourujärvi <jarkko@mourujarvi.net>
2020-07-01 15:12:59 +0300

    4.3: apufunktioita ja yksikkötestejä, step1

--- part4/blogilista/index.js
+++ part4/blogilista/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-const http = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -8,12 +7,12 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  console.log(`Server running on port ${config.PORT}`);
 });
